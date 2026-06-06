114646e052b9049324697eecc83bcac0ee6f040c
GeraldDM21 <gerald.delmon@gmail.com>
2026-06-06 15:52:10 -0600

    feat: inicializar proyecto Spring Boot con Docker

---
+++ src/main/java/cr/cenfotec/focuskids_backend/FocuskidsBackendApplication.java
@@ -0,0 +1,13 @@
+package cr.cenfotec.focuskids_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocuskidsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocuskidsBackendApplication.class, args);
+	}
+
+}
